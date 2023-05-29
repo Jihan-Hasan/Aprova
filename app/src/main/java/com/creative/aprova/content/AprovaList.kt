375e9209548efd0371b2e8cb14c6eb55e98601c4
stk_boss <jihanofficailstk@gmail.com>
2023-05-29 22:19:00 +0600

    Change

--- app/src/main/java/com/creative/aprova/content/AprovaList.kt
+++ app/src/main/java/com/creative/aprova/content/AprovaList.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.creative.aprova.model.Aprovas
@@ -19,12 +22,19 @@ fun AprovaList(
     aprovaData: List<Aprovas>,
     modifier: Modifier = Modifier
 ) {
+//    val list by remember {
+//        mutableListOf(aprovaData)
+//    }
+    val ss by remember {
+        mutableStateOf(aprovaData)
+    }
+
     LazyColumn (
         modifier= Modifier.fillMaxSize(),
         state = rememberLazyListState()
     ){
         itemsIndexed(
-            items = aprovaData,
+            items = ss,
             key = { index, item -> item.id },
             contentType = { index, item -> item::class }
         ) {index, item ->
